4cc92eb53b988c0cc56d26a2b9c0c68739a37c5d
joshsarna <joshsarna@gmail.com>
2018-11-16 20:45:32 -0800

    finish part 2

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+/* global Vue */
+
 new Vue({
   el: '#exercise',
   data: {
-
+    isHighlighted: true
   },
+
   methods: {
     startEffect: function() {
-    
+      var vm = this;
+      setInterval(function() {
+        console.log('again');
+        vm.isHighlighted = !vm.isHighlighted;
+      }, 1000);
     }
-  }
+  },
 });
